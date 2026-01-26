81de8b952dcb9b6ec725aae75fd3715bd04674e5
EDaoren <550790375@qq.com>
2026-01-26 20:00:20 +0800

    重构edaoren站点模板

--- resources/views/edaoren/errors/error.blade.php
+++ resources/views/edaoren/errors/error.blade.php
@@ -7,19 +7,17 @@
             <div class="container">
                 <div class="good-card">
                     <div class="row justify-content-center">
-                        <div class="col-12 col-md-8">
-                            <div class="card mt-3 border-0 shadow-sm">
-                                <div class="card-body text-center p-5">
-                                    <h2 class="mb-3">😣</h2>
-                                    <h4 class="mb-3">{{ $title }}</h4>
-                                    <p class="text-muted mb-4">{{ $content }}</p>
-                                    <div class="mt-4">
-                                        @if(!$url)
-                                            <a href="javascript:history.back(-1);" class="btn btn-outline-primary rounded-pill px-4">{{ __('dujiaoka.callback') }}</a>
-                                        @else
-                                            <a href="{{ $url }}" class="btn btn-outline-primary rounded-pill px-4">{{ __('dujiaoka.callback') }}</a>
-                                        @endif
-                                    </div>
+                        <div class="card mt-3 border-0 shadow-sm">
+                            <div class="card-body text-center p-5">
+                                <h2 class="mb-3">😣</h2>
+                                <h4 class="mb-3">{{ $title }}</h4>
+                                <p class="text-muted mb-4">{{ $content }}</p>
+                                <div class="mt-4">
+                                    @if(!$url)
+                                        <a href="javascript:history.back(-1);" class="btn btn-outline-primary rounded-pill px-4">{{ __('dujiaoka.callback') }}</a>
+                                    @else
+                                        <a href="{{ $url }}" class="btn btn-outline-primary rounded-pill px-4">{{ __('dujiaoka.callback') }}</a>
+                                    @endif
                                 </div>
                             </div>
                         </div>
